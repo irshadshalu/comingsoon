d3b789762596354e44c2cf243ba5cc42b5fc3d05
idealidiot <akshaynathr@outlook.com>
2014-11-14 00:20:25 +0530

    with launch page added functionality

--- wsgi/app/views.py
+++ wsgi/app/views.py
@@ -1,7 +1,14 @@
 from app import app
-from flask import render_template
+from flask import render_template,request
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('home.html')
+	return render_template('base.html')
+
+@app.route('/',methods=['POST'])
+def get_data():
+	t=request.form['email']
+	if t.find('@') !=-1:
+		return "Thank You. We will keep in touch"
+	return "error"
